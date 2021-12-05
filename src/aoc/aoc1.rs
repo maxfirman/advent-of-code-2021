d995327b17cd802f02f5a179df53510374b657e8
Max Firman <max.firman@gmail.com>
2021-12-05 00:43:51 +0000

    cleanup

--- src/aoc/aoc1.rs
+++ src/aoc/aoc1.rs
@@ -15,11 +15,12 @@ pub fn part1() {
 }
 
 pub fn part2() {
+    const N: usize = 3;
     let sum: i32 = read_lines(FILE)
         .unwrap()
         .map(|val| val.unwrap().parse::<i32>().unwrap())
-        .windows::<3>()
-        .map(|[a, b, c]| a + b + c)
+        .windows::<N>()
+        .map(|x| -> i32 {x.iter().sum()})
         .windows()
         .map(|[prev, next]| (next > prev) as i32)
         .sum();

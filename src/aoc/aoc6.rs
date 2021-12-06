a8dbc3884152dda8383f0f0405dd87ac92be538b
Max Firman <max.firman@gmail.com>
2021-12-06 18:03:31 +0000

    aoc6 part 2

--- src/aoc/aoc6.rs
+++ src/aoc/aoc6.rs
@@ -1,13 +1,14 @@
-use crate::io::read_lines;
+use std::collections::HashMap;
 
+use crate::io::read_lines;
 
 const FILE: &str = "data/aoc6.txt";
 
 
-fn part1() -> usize {
+fn part1(days: usize) -> usize {
     let mut nums = read_nums();
 
-    for _ in 0..80 {
+    for _ in 0..days {
         let n = nums
             .iter()
             .filter(|x| **x == 0)
@@ -24,6 +25,33 @@ fn part1() -> usize {
     nums.len()
 }
 
+fn part2(days: usize) -> usize {
+    let mut nums2 = read_nums();
+    let mut nums = nums2.iter().map(|x| *x as usize);
+
+    let mut d1: HashMap<usize, usize> = HashMap::new();
+    let mut d2: HashMap<usize, usize> = HashMap::new();
+
+    for i in 0..=7 {
+        d1.insert(i, 0);
+        d2.insert(i, 0);
+    }
+
+    for num in nums {
+        *d1.get_mut(&num).unwrap() += 1;
+    }
+
+    for i in 0..days {
+        let j = i % 7;
+        let k = (j + 2) % 7;
+        *d2.get_mut(&k).unwrap() += d1[&j];
+        *d1.get_mut(&j).unwrap() += d2[&j];
+        d2.insert(j, 0);
+    }
+
+    return d1.values().sum::<usize>() + d2.values().sum::<usize>();
+}
+
 fn read_nums() -> Vec<i32> {
     read_lines(FILE)
         .unwrap()
@@ -35,8 +63,6 @@ fn read_nums() -> Vec<i32> {
         .collect()
 }
 
-// fn part2() -> usize { 0 }
-
 
 #[cfg(test)]
 mod tests {
@@ -44,12 +70,12 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 352195);
+        assert_eq!(part1(80), 352195);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(), 16925);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(256), 1600306001288);
+    }
 }
 
